e565d675e2056d4e9b2058c0184dee62dab1e3a2
JAYANTH GOWDA C <jayanthgowda17718@gmail.com>
2026-09-13 00:07:41 +0530

    Initial gateway module code setup

---
+++ src/main/java/com/iomp/gateway/IompApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.iomp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IompApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IompApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iomp/gateway/IompApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iomp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IompApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
